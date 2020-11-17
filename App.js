29987653ae4b3872760d86b4338785e108919b7f
zhiguo <zhiguo.guo@joyreach.com>
2020-11-17 23:16:12 +0800

    作业20201116

--- App.js
+++ App.js
@@ -1,138 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  Button,
-  StatusBar,
-  PermissionsAndroid,
-  Platform
+  Switch,
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "请求权限Cool Photo App Camera Permission",
-          message:
-            "Cool Photo App needs access to your camera " +
-            "so you can take awesome pictures.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "取消",
-          buttonPositive: "确定"
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the camera");
-      } else {
-        console.log("Camera permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
-  const [permissionsGranted, setPermissionsGranted] = React.useState(false)
-  function askForPermission() {
-    const Permission = [
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
-    ]
-    if (Platform.OS === 'android') {
-      PermissionsAndroid.requestMultiple(Permission).then(results => {
-        const allPermissionsGranted = Object.values(results).every(result => result === 'granted')
-        if (allPermissionsGranted) {
-          setPermissionsGranted(true)
-        }
-      })
-    } else {
-      setPermissionsGranted(true)
-    }
-  }
+const { width, height } = Dimensions.get('window')
+const cellWith = width * 0.3
+export default function App() {
+  const [isSingle, setIsSingle] = React.useState(false)
+  const [selectedIndex, setSelectedIndex] = React.useState(-1)
   return (
-    <>
-      {/* <StatusBar barStyle="dark-content" /> */}
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>暗号：一次学习，随处编写</Text>
-              <Button title="获取权限" onPress={requestCameraPermission} />
-              <Text style={styles.sectionTitle}>Step One 郭治国</Text>
-              <Button title="获取多项权限" onPress={askForPermission} />
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <View style={styles.container}>
+      <View style={StyleSheet.innerContainer}>
+        <Text>单选</Text>
+        <Switch
+          style={{marginLeft: 10}}
+          value={isSingle}
+          onValueChange={setIsSingle}
+          ></Switch>
+      </View>
+      <View style={styles.innerContainer}>
+        <TouchableOpacity
+          style={{borderColor: 'red', borderWidth: 1}}
+        />
+        { isSingle ?
+          [...new Array(9)].map(i => {
+            return (
+              <TouchableOpacity
+                key={i}
+                onPress={() => setSelectedIndex(i)}
+                style={[styles.cell, selectedIndex === i && {backgroundColor: 'green'}]}
+              />
+            )
+          })
+          :
+          [...new Array(9)].map(i => {
+            return <Cell key={i} />
+          })
+        }
+      </View>
+    </View>
+  )
+}
+
+function Cell() {
+  const [selected, setSelected] = React.useState(false)
+  return (
+    <TouchableOpacity
+      onPress={() => setSelected(!selected)}
+      style={[styles.cell, seleted && { backgroundColor: 'green'}]}
+    />
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {},
+  innerContainer: {
+    marginTop: 100,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  cell: {
+    width: cellWidth,
+    height: cellWidth,
+    borderWidth: 1,
+    borderColor: 'block'
   },
 });
-
-export default App;
